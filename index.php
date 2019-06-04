eff5279ad1a8e12e83245d443cf591743c5a3652
alumno informatica <alumno@ciis.usil.edu.pe>
2019-06-04 10:01:07 -0500

    version 3

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+$pdo = new PDO("mysql:host=localhost;dbname=blog;charset=utf8","root","");
+$query = "SELECT * FROM entradas ORDER BY id DESC";
+$entradas = $pdo->query($query)->fetchAll();
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +19,11 @@
 </head>
 <body>
     <h1>Aplicacion PHP con MYSQL</h1>
+    <?php foreach ($entradas as $e){ ?>
+    
+    <h2><?php echo $e["titulo"] ?></h2>
+    <span>Autor: <?php echo $e["autor"]?></span>
+    <p><?php echo $e["contenido"]?></p>
+    <?php } ?>
 </body>
 </html>
